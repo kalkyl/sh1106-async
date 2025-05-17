265b295aa23bf910e6f80dee6c48679583d25a51
kalkyl <henrik.alser@me.com>
2025-05-17 16:08:21 +0200

    :rainbow:

---
+++ src/lib.rs
@@ -0,0 +1,104 @@
+//! sh1106 OLED display driver
+//!
+//! The driver must be initialised by passing an I2C or SPI interface peripheral to the
+//! [`Builder`](builder/struct.Builder.html),
+//! which will in turn create a driver instance in a particular mode. By default, the builder
+//! returns a `mode::RawMode` instance which isn't very useful by itself. You can coerce the driver
+//! into a more useful mode by calling `into()` and defining the type you want to coerce to. For
+//! example, to initialise the display with an I2C interface and
+//! [`mode::GraphicsMode`](mode/graphics/struct.GraphicsMode.html), you would do something like
+//! this:
+//!
+//! ```rust,no_run
+//! use sh1106::{prelude::*, Builder};
+//! # let i2c = sh1106::test_helpers::I2cStub;
+//!
+//! let mut display: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
+//!
+//! display.init().unwrap();
+//! display.flush().unwrap();
+//!
+//! display.set_pixel(10, 20, 1);
+//!
+//! display.flush().unwrap();
+//! ```
+//!
+//! See the [example](https://github.com/jamwaffles/sh1106/blob/master/examples/graphics_i2c.rs)
+//! for more usage. The [entire `embedded_graphics` featureset](https://github.com/jamwaffles/embedded-graphics#features)
+//! is supported by this driver.
+//!
+//! It's possible to customise the driver to suit your display/application. Take a look at the
+//! [Builder] for available options.
+//!
+//! # Examples
+//!
+//! Examples can be found in
+//! [the examples/ folder](https://github.com/jamwaffles/sh1106/blob/master/examples)
+//!
+//! ## Draw some text to the display
+//!
+//! Uses [mode::GraphicsMode] and [embedded_graphics](../embedded_graphics/index.html).
+//!
+//! ```rust,no_run
+//! use embedded_graphics::{
+//!     mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
+//!     pixelcolor::BinaryColor,
+//!     prelude::*,
+//!     text::{Baseline, Text},
+//! };
+//! use sh1106::{prelude::*, Builder};
+//! # let i2c = sh1106::test_helpers::I2cStub;
+//!
+//! let mut display: GraphicsMode<_> = Builder::new().connect_i2c(i2c).into();
+//!
+//! display.init().unwrap();
+//! display.flush().unwrap();
+//!
+//! let text_style = MonoTextStyleBuilder::new()
+//!     .font(&FONT_6X10)
+//!     .text_color(BinaryColor::On)
+//!     .build();
+//!
+//! Text::with_baseline("Hello world!", Point::zero(), text_style, Baseline::Top)
+//!     .draw(&mut display)
+//!     .unwrap();
+//!
+//! Text::with_baseline("Hello Rust!", Point::new(0, 16), text_style, Baseline::Top)
+//!     .draw(&mut display)
+//!     .unwrap();
+//!
+//! display.flush().unwrap();
+//! ```
+#![no_std]
+#![feature(type_alias_impl_trait, async_fn_in_trait, impl_trait_projections)]
+#![allow(incomplete_features)]
+#![deny(missing_docs)]
+#![deny(missing_copy_implementations)]
+#![deny(trivial_casts)]
+#![deny(trivial_numeric_casts)]
+#![deny(unsafe_code)]
+// #![deny(unstable_features)]
+#![deny(unused_import_braces)]
+#![deny(unused_qualifications)]
+
+/// Errors in this crate
+#[derive(Debug)]
+pub enum Error<CommE, PinE> {
+    /// Communication error
+    Comm(CommE),
+    /// Pin setting error
+    Pin(PinE),
+}
+
+extern crate embedded_hal as hal;
+
+pub mod builder;
+mod command;
+pub mod displayrotation;
+pub mod displaysize;
+pub mod interface;
+pub mod mode;
+pub mod prelude;
+pub mod properties;
+
+pub use crate::builder::Builder;
---
+++ src/properties.rs
@@ -0,0 +1,170 @@
+//! Container to store and set display properties
+
+use crate::{
+    command::{Command, VcomhLevel},
+    displayrotation::DisplayRotation,
+    displaysize::DisplaySize,
+    interface::DisplayInterface,
+};
+
+/// Display properties struct
+pub struct DisplayProperties<DI> {
+    iface: DI,
+    display_size: DisplaySize,
+    display_rotation: DisplayRotation,
+    draw_area_start: (u8, u8),
+    draw_area_end: (u8, u8),
+    draw_column: u8,
+    draw_row: u8,
+}
+
+impl<DI> DisplayProperties<DI>
+where
+    DI: DisplayInterface,
+{
+    /// Create new DisplayProperties instance
+    pub fn new(
+        iface: DI,
+        display_size: DisplaySize,
+        display_rotation: DisplayRotation,
+    ) -> DisplayProperties<DI> {
+        DisplayProperties {
+            iface,
+            display_size,
+            display_rotation,
+            draw_area_start: (0, 0),
+            draw_area_end: (0, 0),
+            draw_column: 0,
+            draw_row: 0,
+        }
+    }
+
+    /// Initialise the display in column mode (i.e. a byte walks down a column of 8 pixels) with
+    /// column 0 on the left and column _(display_width - 1)_ on the right.
+    pub async fn init_column_mode(&mut self) -> Result<(), DI::Error> {
+        self.iface.init()?;
+        // TODO: Break up into nice bits so display modes can pick whathever they need
+        let (_, display_height) = self.display_size.dimensions();
+        let display_rotation = self.display_rotation;
+
+        Command::DisplayOn(false).send(&mut self.iface).await?;
+        Command::DisplayClockDiv(0x8, 0x0).send(&mut self.iface).await?;
+        Command::Multiplex(display_height - 1).send(&mut self.iface).await?;
+        Command::DisplayOffset(0).send(&mut self.iface).await?;
+        Command::StartLine(0).send(&mut self.iface).await?;
+        // TODO: Ability to turn charge pump on/off
+        // Display must be off when performing this command
+        Command::ChargePump(true).send(&mut self.iface).await?;
+
+        self.set_rotation(display_rotation).await?;
+
+        match self.display_size {
+            DisplaySize::Display128x32 => Command::ComPinConfig(false).send(&mut self.iface).await,
+            DisplaySize::Display128x64
+            | DisplaySize::Display128x64NoOffset
+            | DisplaySize::Display132x64 => Command::ComPinConfig(true).send(&mut self.iface).await,
+        }?;
+
+        Command::Contrast(0x80).send(&mut self.iface).await?;
+        Command::PreChargePeriod(0x1, 0xF).send(&mut self.iface).await?;
+        Command::VcomhDeselect(VcomhLevel::Auto).send(&mut self.iface).await?;
+        Command::AllOn(false).send(&mut self.iface).await?;
+        Command::Invert(false).send(&mut self.iface).await?;
+        Command::DisplayOn(true).send(&mut self.iface).await?;
+
+        Ok(())
+    }
+
+    /// Set the position in the framebuffer of the display where any sent data should be
+    /// drawn. This method can be used for changing the affected area on the screen as well
+    /// as (re-)setting the start point of the next `draw` call.
+    pub async fn set_draw_area(&mut self, start: (u8, u8), end: (u8, u8)) -> Result<(), DI::Error> {
+        self.draw_area_start = start;
+        self.draw_area_end = end;
+        self.draw_column = start.0;
+        self.draw_row = start.1;
+
+        self.send_draw_address().await
+    }
+
+    /// Send the data to the display for drawing at the current position in the framebuffer
+    /// and advance the position accordingly. Cf. `set_draw_area` to modify the affected area by
+    /// this method.
+    pub async fn draw(&mut self, mut buffer: &[u8]) -> Result<(), DI::Error> {
+        while !buffer.is_empty() {
+            let count = self.draw_area_end.0 - self.draw_column;
+            self.iface.send_data(&buffer[..count as usize]).await?;
+            self.draw_column += count;
+
+            if self.draw_column >= self.draw_area_end.0 {
+                self.draw_column = self.draw_area_start.0;
+
+                self.draw_row += 8;
+                if self.draw_row >= self.draw_area_end.1 {
+                    self.draw_row = self.draw_area_start.1;
+                }
+
+                self.send_draw_address().await?;
+            }
+
+            buffer = &buffer[count as usize..];
+        }
+
+        Ok(())
+    }
+
+    async fn send_draw_address(&mut self) -> Result<(), DI::Error> {
+        Command::PageAddress(self.draw_row.into()).send(&mut self.iface).await?;
+        Command::ColumnAddressLow(0xF & self.draw_column).send(&mut self.iface).await?;
+        Command::ColumnAddressHigh(0xF & (self.draw_column >> 4)).send(&mut self.iface).await
+    }
+
+    /// Get the configured display size
+    pub fn get_size(&self) -> DisplaySize {
+        self.display_size
+    }
+
+    /// Get display dimensions, taking into account the current rotation of the display
+    pub fn get_dimensions(&self) -> (u8, u8) {
+        let (w, h) = self.display_size.dimensions();
+
+        match self.display_rotation {
+            DisplayRotation::Rotate0 | DisplayRotation::Rotate180 => (w, h),
+            DisplayRotation::Rotate90 | DisplayRotation::Rotate270 => (h, w),
+        }
+    }
+
+    /// Get the display rotation
+    pub fn get_rotation(&self) -> DisplayRotation {
+        self.display_rotation
+    }
+
+    /// Set the display rotation
+    pub async fn set_rotation(&mut self, display_rotation: DisplayRotation) -> Result<(), DI::Error> {
+        self.display_rotation = display_rotation;
+
+        match display_rotation {
+            DisplayRotation::Rotate0 => {
+                Command::SegmentRemap(true).send(&mut self.iface).await?;
+                Command::ReverseComDir(true).send(&mut self.iface).await
+            }
+            DisplayRotation::Rotate90 => {
+                Command::SegmentRemap(false).send(&mut self.iface).await?;
+                Command::ReverseComDir(true).send(&mut self.iface).await
+            }
+            DisplayRotation::Rotate180 => {
+                Command::SegmentRemap(false).send(&mut self.iface).await?;
+                Command::ReverseComDir(false).send(&mut self.iface).await
+            }
+            DisplayRotation::Rotate270 => {
+                Command::SegmentRemap(true).send(&mut self.iface).await?;
+                Command::ReverseComDir(false).send(&mut self.iface).await
+            }
+        }
+    }
+
+    /// Set the display contrast
+    pub async fn set_contrast(&mut self, contrast: u8) -> Result<(), DI::Error> {
+        Command::Contrast(contrast).send(&mut self.iface).await
+    }
+}
